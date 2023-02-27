70c7faef9897f960772383770010a60b1e5effd0
PIYUSH MAKWANA <mpiyush3510@cbigujarat.onmicrosoft.com>
2023-02-27 19:38:18 +0530

    Break;

---
+++ app/src/main/java/com/mpiyush3510/studentlogin/Keys.java
@@ -0,0 +1,10 @@
+package com.mpiyush3510.studentlogin;
+
+public class Keys {
+    public static final String KEY_NAME="name";
+    public static final String KEY_EMAIL="email";
+    public static final String KEY_MNO="phone";
+    public static final String KEY_COURSE="course";
+    public static final String KEY_COLLECTIONS_USER="users";
+    public static final String KEY_PASSWORD="password";
+}
