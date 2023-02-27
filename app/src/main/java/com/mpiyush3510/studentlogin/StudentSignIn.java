2a814bb30c69c1fbba865a54f110b23b877b7ba3
PIYUSH MAKWANA <mpiyush3510@cbigujarat.onmicrosoft.com>
2023-02-27 18:08:34 +0530

    signIn_Ui Completed

---
+++ app/src/main/java/com/mpiyush3510/studentlogin/StudentSignIn.java
@@ -0,0 +1,13 @@
+package com.mpiyush3510.studentlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentSignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_sign_in);
+    }
+}
